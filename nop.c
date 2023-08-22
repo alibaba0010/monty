d01fe8a54a9f4cf53f3c8fe10a9d90b81f3044e4
root <zakariyahali100@gmail.com>
2023-08-21 23:35:52 -0700

    add

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_nop - No operation function
+ * @stack: Pointer to the head of the stack
+ * @line_number: Line number in the input file
+ * Return: No return value
+ */
+void f_nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
+
