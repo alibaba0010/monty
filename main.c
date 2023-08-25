dc55f1ce60c7f67b8dbfa5e285c6b7ec3142cbda
root <zakariyahali100@gmail.com>
2023-08-24 17:04:07 -0700

    compile

--- main.c
+++ main.c
@@ -1,46 +1,54 @@
+#define  _POSIX_C_SOURCE 200809L
+#include <stdio.h>
 #include "monty.h"
-bus_t final_bus = {NULL, NULL, NULL, 0};
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - Monty code interpreter
- * @argc: count of arguments
- * @argv: Monty file location
- * Return: 0 on success
- */
+* main - interprets Monty code
+* @argc: number of arguments
+* @argv: location of Monty file
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-	char *file_content;
-	FILE *file_ptr;
-	size_t content_size = 0;
-	ssize_t read_result = 1;
-	stack_t *final_stack = NULL;
-	unsigned int line_counter = 0;
+    char *file_content;
+    FILE *monty_file;
+    size_t content_size = 0;
+    ssize_t line_read = 1;
+    stack_t *stack = NULL;
+    unsigned int line_number = 0;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file_ptr = fopen(argv[1], "r");
-	final_bus.file = file_ptr;
-	if (!file_ptr)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_result > 0)
-	{
-		file_content = NULL;
-		read_result = getline(&file_content, &content_size, file_ptr);
-		final_bus.content = file_content;
-		line_counter++;
-		if (read_result > 0)
-		{
-			execute(file_content, &final_stack, line_counter, file_ptr);
-		}
-		free(file_content);
-	}
-	free_stack(final_stack);
-	fclose(file_ptr);
-	return (0);
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    monty_file = fopen(argv[1], "r");
+    bus.file = monty_file;
+
+    if (!monty_file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (line_read > 0)
+    {
+        file_content = NULL;
+        line_read = getline(&file_content, &content_size, monty_file);
+        bus.content = file_content;
+        line_number++;
+
+        if (line_read > 0)
+        {
+            execute(file_content, &stack, line_number, monty_file);
+        }
+
+        free(file_content);
+    }
+
+    free_stack(stack);
+    fclose(monty_file);
+    return (0);
 }
 
